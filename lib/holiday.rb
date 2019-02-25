264bfb977f74b7be5891e83de82c46a718d5e529
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-25 21:00:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,8 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_supplies[:winter][:new_years] << "Wreath"
-  holiday_supplies[:winter][:new_years] << "Lights"
+  holiday_supplies[:winter][:new_years] << supply
 end
 
 def all_supplies_in_holidays(holiday_hash)
