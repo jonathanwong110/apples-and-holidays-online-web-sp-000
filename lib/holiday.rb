daa5c9059295a9dba316fe7378fd2aacc7b0d8e9
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-25 20:56:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday
+  holiday.hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    
 end
 
 def all_holidays_with_bbq(holiday_hash)
