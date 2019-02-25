fa9c0886fdd1c03c64cefc6eb30e77a88a957c07
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-25 21:03:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday.hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "#{holiday.map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
